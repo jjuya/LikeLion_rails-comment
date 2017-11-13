9e2a389272b5275b2c47efc0675ca62377a6acf6
jjuya <jjuya9381@naver.com>
2017-11-13 08:20:27 +0000

    comment practice

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,40 @@
+class PostController < ApplicationController
+  def index # 게시글 리스트
+    @posts = Post.all
+  end
+
+  def new # 게시글 작성 form
+  end
+
+  def create # 작성된 게시글을 DB에 넣는 액션
+    title = params[:title]
+    content = params[:content]
+    
+    Post.create(
+      title: title,
+      content: content
+    )
+    
+    redirect_to '/'
+  end
+
+  def show # 게시글 상세보기
+    id = params[:id]
+    
+    @post = Post.find(id)
+    
+    @comments = @post.comments
+  end
+
+  def create_commnet # 게시글에 댓글 다는 액션
+    post_id = params[:post_id]
+    content = params[:content]
+    
+    Comment.create(
+      post_id: post_id,
+      content: content
+    )
+    
+    redirect_to :back
+  end
+end
